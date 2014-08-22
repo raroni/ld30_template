22cd7f781508d5e14fe77966b0455e637cff165e
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2014-08-22 22:59:59 +0200

    Added gradient bg effect.

--- game/renderer.js
+++ game/renderer.js
@@ -6,12 +6,15 @@ define(function() {
     eventBus.subscribe('entityCreation', this.handleCreation, this);
 
     this.entities = [];
+
+    this.backgroundGradient = this.context.createRadialGradient(100, 100, 10, 100, 100, 600);
+    this.backgroundGradient.addColorStop(0, '#8ED6FF');
+    this.backgroundGradient.addColorStop(1, '#004CB3');
   }
 
   Renderer.prototype = {
     draw: function() {
-      this.context.fillStyle = '#fff';
-      this.context.fillRect(-this.canvasElement.width*0.5, -this.canvasElement.height*0.5, this.canvasElement.width, this.canvasElement.height);
+      this.clear();
 
       this.entities.forEach(function(entity) {
         var aspects = entity.aspects;
@@ -38,6 +41,10 @@ define(function() {
       if(entity.aspects.shapeRendering) {
         this.entities.push(entity);
       }
+    },
+    clear: function() {
+      this.context.fillStyle = this.backgroundGradient;
+      this.context.fillRect(-this.canvasElement.width*0.5, -this.canvasElement.height*0.5, this.canvasElement.width, this.canvasElement.height);
     }
   };
 
